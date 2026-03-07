64bb94765defef8d71f22d969ab7f93549de0e3b
Bekzod Allaev <Allaev_@outlook.com>
2026-03-07 08:41:02 +0100

    Add CurrencyApiResponse model for currency API responses
    Introduced CurrencyApiResponse.cs in FabulousBackendAlgorithms.Models.
    This class defines a Data property (Dictionary<string, decimal>)
    mapped to the "data" JSON field, enabling structured handling
    of currency API responses.

---
+++ FabulousBackendAlgorithms/Models/CurrencyApiResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FabulousBackendAlgorithms.Models
+{
+    public class CurrencyApiResponse
+    {
+        [JsonPropertyName("data")]
+        public Dictionary<string, decimal> Data { get; init; } = new();
+    }
+}
