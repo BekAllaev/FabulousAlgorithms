abc77b85faaa1bd373958d60caf7b79ca3cfa0a5
Bekzod Allaev <Allaev_@outlook.com>
2026-06-18 08:13:44 +0200

    Add permutation utilities and extension methods
    Added utility extension methods for collections and permutations in Extensions.cs, such as CartesianProduct, Swap, and Factorial. Introduced Permutations.cs with GetLexiPermutation for lexicographical permutations. Improved code formatting and consistency.

--- FabulousAlgorithms/Common/Extensions.cs
+++ FabulousAlgorithms/Common/Extensions.cs
@@ -2,6 +2,7 @@
 using FabulousAlgorithms.ImmutableStack.Covariant;
 using FabulousAlgorithms.ImmutableStack.Extensions;
 using FabulousAlgorithms.Life;
+using System.Numerics;
 
 namespace FabulousAlgorithms.Common;
 
@@ -16,16 +17,16 @@ public static class Extensions
         this IImmutableStackCovariant<T> stack, IImmutableStackCovariant<T> tail)
     {
         var result = tail;
-        for (;!stack.IsEmpty;stack = stack.Pop())
+        for (; !stack.IsEmpty; stack = stack.Pop())
             result = result.Push(stack.Peek());
         return result;
     }
 
-    public static IImmutableStackCovariant<T> Reverse<T>(this IImmutableStackCovariant<T> stack) 
+    public static IImmutableStackCovariant<T> Reverse<T>(this IImmutableStackCovariant<T> stack)
         => stack.ReverseOnto(ImmutableStackCovariant<T>.Empty);
 
     public static IImmutableStackCovariant<T> Concatenate<T>(this IImmutableStackCovariant<T> xs, IImmutableStackCovariant<T> ys)
-        => ys.IsEmpty? xs : xs.Reverse().ReverseOnto(ys);
+        => ys.IsEmpty ? xs : xs.Reverse().ReverseOnto(ys);
 
     public static IImmutableStackCovariant<T> Append<T>(this IImmutableStackCovariant<T> stack, T item)
         => stack.Concatenate(ImmutableStackCovariant<T>.Empty.Push(item));
@@ -58,7 +59,7 @@ public static class Extensions
         long w = quad.Width() / 4; // /4 because we want half-width of sub-quad
 
         QuadPoint newp = ( //This is the tricky bit!
-            quad.Level == 1 ? 0 : 0 <= p.X ? -w : p.X + w, 
+            quad.Level == 1 ? 0 : 0 <= p.X ? -w : p.X + w,
             quad.Level == 1 ? 0 : 0 <= p.Y ? p.Y - w : p.Y + w);
 
         if (0 <= p.X)
@@ -68,7 +69,7 @@ public static class Extensions
                 return quad.SE.Get(newp);
         else if (0 <= p.Y)
             return quad.NW.Get(newp);
-        else 
+        else
             return quad.SW.Get(newp);
     }
 
@@ -127,29 +128,103 @@ public static class Extensions
     public static bool HasAllEmptyEdges(this IQuad quad) =>
         quad.NW.NW.IsEmpty &&
         quad.NW.NE.IsEmpty &&
-        quad.NE.NW.IsEmpty && 
+        quad.NE.NW.IsEmpty &&
         quad.NE.NE.IsEmpty &&
-        quad.NE.SE.IsEmpty && 
+        quad.NE.SE.IsEmpty &&
         quad.SE.NE.IsEmpty &&
-        quad.SE.SE.IsEmpty && 
+        quad.SE.SE.IsEmpty &&
         quad.SE.SW.IsEmpty &&
-        quad.SW.SE.IsEmpty && 
+        quad.SW.SE.IsEmpty &&
         quad.SW.SW.IsEmpty &&
-        quad.SW.NW.IsEmpty && 
+        quad.SW.NW.IsEmpty &&
         quad.NW.SW.IsEmpty;
 
     public static IQuad Center(this IQuad quad) =>
         Quad.Make(quad.NW.SE, quad.NE.SW, quad.SE.NW, quad.SW.NE);
-    
+
     public static IQuad North(this IQuad quad) =>
         Quad.Make(quad.NW.NE, quad.NE.NW, quad.NE.SW, quad.NW.SE);
-    
+
     public static IQuad East(this IQuad quad) =>
         Quad.Make(quad.NE.SW, quad.NE.SE, quad.SE.NE, quad.SE.NW);
-    
+
     public static IQuad South(this IQuad quad) =>
         Quad.Make(quad.SW.NE, quad.SE.NW, quad.SE.SW, quad.SW.SE);
-    
+
     public static IQuad West(this IQuad quad) =>
         Quad.Make(quad.NW.SW, quad.NW.SE, quad.SW.NE, quad.SW.NW);
+
+    public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(this IEnumerable<IEnumerable<T>> sequences)
+    {
+        IEnumerable<IDeque<T>> result = [Deque<T>.Empty];
+
+        foreach (var sequence in sequences)
+            result = from deque in result
+                     from item in sequence
+                     select deque.PushRight(item);
+
+        return result;
+    }
+
+    /// <summary>
+    /// This method do the same job as <see cref="CartesianProduct{T}(IEnumerable{IEnumerable{T}})"/>
+    /// but the only differences is that it do without the loop that mentioned method has. It does using
+    /// Aggregate method from basic library
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="sequences"></param>
+    /// <returns></returns>
+    public static IEnumerable<IEnumerable<T>> CartesianProductWithAppend<T>
+        (this IEnumerable<IEnumerable<T>> sequences) =>
+        sequences.Aggregate(
+            (IEnumerable<IDeque<T>>)[Deque<T>.Empty],
+            (accumulator, sequence) =>
+                from deque in accumulator
+                from item in sequence
+                select deque.PushRight(item));
+
+    public static string Concat<T>(this IEnumerable<T> items) =>
+        string.Join("", items);
+
+    public static bool Greater<T>(this IList<T> items, int x, int y) where T : IComparable<T>
+        => items[x].CompareTo(items[y]) > 0;
+
+    public static void Swap<T>(this IList<T> items, int x, int y) => (items[x], items[y]) = (items[y], items[x]);
+
+    public static void ReverseRange<T>(this IList<T> items, int start, int end)
+    {
+        for (; start < end; start += 1, end -= 1)
+            items.Swap(start, end);
+    }
+
+    public static bool NextLexiPremutation<T>(this IList<T> items) where T: IComparable<T>
+    {
+        int first = items.Count - 2;
+
+        while (first >= 0 && !items.Greater(first + 1, first))
+            first -= 1;
+
+        if (first < 0)
+            return false;
+
+        int second = items.Count - 1;
+
+        while (!items.Greater(second, first))
+            second -= 1;
+
+        items.Swap(first, second);
+        items.ReverseRange(first + 1, items.Count - 1);
+
+        return true;
+    }
+
+    public static BigInteger Factorial(this int n)
+    {
+        BigInteger result = 1;
+
+        for (int i = 2; i <= n; i += 1)
+            result *= i;
+
+        return result;
+    }
 }
---
+++ FabulousAlgorithms/Permutations/Permutations.cs
@@ -0,0 +1,28 @@
+﻿using FabulousAlgorithms.Common;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace FabulousAlgorithms.Permutations;
+
+public class Permutations
+{
+    static IList<int> GetLexiPermutation(int n, BigInteger p)
+    {
+        var remaining = new List<int>(Enumerable.Range(0, n));
+        var result = new List<int>(n);
+        var f = n.Factorial();
+
+        for (int cur = n; cur > 0; cur -= 1)
+        {
+            f /= cur;
+            var r = (int)(p / f);
+            p %= f;
+            result.Add(remaining[r]);
+            remaining.RemoveAt(r);
+        }
+
+        return result;
+    }
+}
